acaab5c43c394e37c75f56c0d0b2c0f52ee32c0a
yugmani <yugmani@yahoo.com>
2021-07-05 23:58:32 -0700

    promise with .then() and .catch()

--- async-await.js
+++ async-await.js
@@ -1,11 +1,18 @@
+// ************************************
+// Understanding async and await in JavaScript
+// ***********************************
+
+// Promises using .then() and .catch();
 let flag = true;
+// let flag = false;  // uncomment this line and commet out line 6 to see different case.
 let promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    if (flag) resoleve('I am resolved');
+    if (flag) resolve('I am resolved');
     else reject(new Error('I am rejected'));
   }, 1000);
 });
 
-promise.then(res => {
-  console.log(res).catch(err => console.log(err));
-});
+promise.then(res => console.log(res)).catch(err => console.log(err));
+
+// I am resolved
+// if you set `let flag = false` it will result => `Error: I am rejected`
